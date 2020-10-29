57ddc169b104426207a3ab4d4373128b986ea0fb
jose30caycho@gmail.com <soni3022>
2020-10-28 23:10:16 -0500

    ss

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,187 +1,65 @@
-import React, {Component} from 'react'
-import { Table, Button, Space } from 'antd';
+import React, {Component} from 'react';
+import 'antd/dist/antd.css';
+import { Table, Space } from 'antd';
 import axios from 'axios';
 
-const data = [
+const columns = [
   {
-    key: 'CEO',
-    name: 'Producto',
-    sub_name: 32,
-    cola: 1,
-    nivel: 1,
-    sub_div: 1,
-    embaj: 'nuevo'
+    title: 'Codigo',
+    dataIndex: 'code',
+    key: 'code',
+    render: text => <a>{text}</a>,
   },
   {
-    key: 'CEO',
-    name: 'Producto',
-    sub_name: 32,
-    cola: 1,
-    nivel: 1,
-    sub_div: 1,
-    embaj: 'nuevo'
+    title: 'Nombre',
+    dataIndex: 'name',
+    key: 'name',
   },
   {
-    key: 'CEO',
-    name: 'Producto',
-    sub_name: 32,
-    cola: 1,
-    nivel: 1,
-    sub_div: 1,
-    embaj: 'nuevo'
+    title: 'Descripcion',
+    dataIndex: 'description',
+    key: 'description',
   },
   {
-    key: 'CEO',
-    name: 'Producto',
-    sub_name: 32,
-    cola: 1,
-    nivel: 1,
-    sub_div: 1,
-    embaj: 'nuevo'
+    title: 'Precio',
+    dataIndex: 'price',
+    key: 'price',
+  },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size="middle">
+        <a>Edit</a>
+        <a>Delete</a>
+      </Space>
+    ),
   },
 ];
 
+
 export default class Grid extends Component {
-
-
-
   state = {
-    filteredInfo: null,
-    sortedInfo: null,
-    data:[]
+    data: []
   };
 
   componentDidMount(){
       axios.get('http://localhost:2014/api/v1/areas')
-      .then(res => {
-          console.log(res)
+      .then(response => {
+        console.log(response)
+          this.setState({
+            data: response.data.data
+          });
       })
       .catch(err => {
           console.error(err); 
-      })
-  }
-
-  handleChange = (pagination, filters, sorter) => {
-    console.log('Various parameters', pagination, filters, sorter);
-    this.setState({
-      filteredInfo: filters,
-      sortedInfo: sorter,
-    });
-  };
-
-  clearFilters = () => {
-    this.setState({ filteredInfo: null });
-  };
-
-  clearAll = () => {
-    this.setState({
-      filteredInfo: null,
-      sortedInfo: null,
-    });
-  };
-
-  setAgeSort = () => {
-    this.setState({
-      sortedInfo: {
-        order: 'descend',
-        columnKey: 'age',
-      },
-    });
+      });
   };
 
   render() {
-    let { sortedInfo, filteredInfo } = this.state;
-    sortedInfo = sortedInfo || {};
-    filteredInfo = filteredInfo || {};
-    const columns = [
-      {
-        title: 'Division',
-        dataIndex: 'name',
-        key: 'name',
-        filters: [
-          { text: 'Joe', value: 'Joe' },
-          { text: 'Jim', value: 'Jim' },
-        ],
-        filteredValue: filteredInfo.name || null,
-        onFilter: (value, record) => record.name.includes(value),
-        sorter: (a, b) => a.name.length - b.name.length,
-        sortOrder: sortedInfo.columnKey === 'name' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Division superior',
-        dataIndex: 'sub_name',
-        key: 'sub_name',
-        sorter: (a, b) => a.age - b.age,
-        sortOrder: sortedInfo.columnKey === 'sub_name' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Colaboradores',
-        dataIndex: 'cola',
-        key: 'cola',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'cola' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Nivel',
-        dataIndex: 'nivel',
-        key: 'nivel',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'nivel' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Subdivisiones',
-        dataIndex: 'sub_div',
-        key: 'sub_div',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'sub_div' && sortedInfo.order,
-        ellipsis: true,
-      },
-      {
-        title: 'Embajadores',
-        dataIndex: 'embaj',
-        key: 'embaj',
-        filters: [
-          { text: 'London', value: 'London' },
-          { text: 'New York', value: 'New York' },
-        ],
-        filteredValue: filteredInfo.address || null,
-        onFilter: (value, record) => record.address.includes(value),
-        sorter: (a, b) => a.address.length - b.address.length,
-        sortOrder: sortedInfo.columnKey === 'embaj' && sortedInfo.order,
-        ellipsis: true,
-      },
-    ];
-    return (
-      <>
-        <Space style={{ marginBottom: 16 }}>
-          <Button onClick={this.setAgeSort}>Sort age</Button>
-          <Button onClick={this.clearFilters}>Clear filters</Button>
-          <Button onClick={this.clearAll}>Clear filters and sorters</Button>
-        </Space>
-        <Table columns={columns} dataSource={data} onChange={this.handleChange} pagination={{ pageSize: 2 }} />
-      </>
-    );
-  }
+    const {data} = this.state;
+    return(
+      <Table columns={columns} dataSource={data}  />
+    )
+  };
 }

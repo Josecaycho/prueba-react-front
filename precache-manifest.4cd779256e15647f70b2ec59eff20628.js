e097597383f3e116fd5c7614dbc9afc6ad733f32
JoseCaycho <jose30caycho@gmail.com>
2022-09-30 04:26:00 -0500

    Updates

---
+++ precache-manifest.4cd779256e15647f70b2ec59eff20628.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da3f37e587dca1654305e9d451721f3",
+    "url": "/Josecaycho/prueba-react-front/index.html"
+  },
+  {
+    "revision": "44ef2829e93b75ed9921",
+    "url": "/Josecaycho/prueba-react-front/static/css/main.bece5b40.chunk.css"
+  },
+  {
+    "revision": "9afbcf94350292835c7e",
+    "url": "/Josecaycho/prueba-react-front/static/js/2.6a26f9d1.chunk.js"
+  },
+  {
+    "revision": "746836c0f054e75c704a695f33fda7bc",
+    "url": "/Josecaycho/prueba-react-front/static/js/2.6a26f9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44ef2829e93b75ed9921",
+    "url": "/Josecaycho/prueba-react-front/static/js/main.2d16fcca.chunk.js"
+  },
+  {
+    "revision": "b502ae8c30f75859e0b6",
+    "url": "/Josecaycho/prueba-react-front/static/js/runtime-main.679c837a.js"
+  },
+  {
+    "revision": "894a5299db640b64a0d09bbf5e162285",
+    "url": "/Josecaycho/prueba-react-front/static/media/arrowTabs.894a5299.svg"
+  },
+  {
+    "revision": "e9823cf1f782e3bd35f4fe99a1502915",
+    "url": "/Josecaycho/prueba-react-front/static/media/check.e9823cf1.svg"
+  },
+  {
+    "revision": "2cc9e9e7312efb2e3f69107ac005b979",
+    "url": "/Josecaycho/prueba-react-front/static/media/fondo.2cc9e9e7.png"
+  },
+  {
+    "revision": "a66e46da2c754c3fb9a34d4a2c2e9cad",
+    "url": "/Josecaycho/prueba-react-front/static/media/persona-responsive.a66e46da.svg"
+  },
+  {
+    "revision": "d5c5078f830530014f2239ac8b7f90f9",
+    "url": "/Josecaycho/prueba-react-front/static/media/persona1.d5c5078f.svg"
+  },
+  {
+    "revision": "4b5ede9a3e632dbb611425080da3542a",
+    "url": "/Josecaycho/prueba-react-front/static/media/persona2.4b5ede9a.svg"
+  },
+  {
+    "revision": "15e4835e71dd8187a83dc8b67741b697",
+    "url": "/Josecaycho/prueba-react-front/static/media/retroceder.15e4835e.svg"
+  },
+  {
+    "revision": "5ad79e634a537943f11fc584c9290188",
+    "url": "/Josecaycho/prueba-react-front/static/media/telefono.5ad79e63.svg"
+  }
+]);
